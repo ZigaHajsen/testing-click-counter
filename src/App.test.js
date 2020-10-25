3c4cac748813885ff1384691d1754f06cfb07fe0
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-25 17:01:05 +0100

    App Component test

--- src/App.test.js
+++ src/App.test.js
@@ -7,4 +7,12 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without error', () => {});
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+test('renders button', () => {});
+test('renders counter display', () => {});
+test('counter starts at 0', () => {});
+test('clicking on button increments counter display', () => {});
