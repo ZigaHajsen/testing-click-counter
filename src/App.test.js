9c37a1972a9611d71c5508e66e5734ff6ccca799
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-25 17:22:43 +0100

    Refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -7,19 +7,24 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = () => shallow(<App />);
+
+const findByTestAttribute = (wrapper, value) =>
+  wrapper.find(`[data-test='${value}']`);
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttribute(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 test('renders button', () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAttribute(wrapper, 'increment-button');
   expect(button.length).toBe(1);
 });
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttribute(wrapper, 'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 test('counter starts at 0', () => {});
