ba14253a7b0935c4efe1ef68aea7a0781464a80f
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-25 17:07:22 +0100

    Counter Display & Increment Button

--- src/App.test.js
+++ src/App.test.js
@@ -12,7 +12,15 @@ test('renders without error', () => {
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
-test('renders button', () => {});
-test('renders counter display', () => {});
+test('renders button', () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+test('renders counter display', () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
 test('counter starts at 0', () => {});
 test('clicking on button increments counter display', () => {});
